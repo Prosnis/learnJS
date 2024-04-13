1c44ca64c050915a1d6819f782ffbfd2271f7bed
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-04-13 22:59:39 +0700

    Update 4.1.js

--- Объекты: основы/4.1.js
+++ Объекты: основы/4.1.js
@@ -1,3 +1,43 @@
+/ задача "Робот"
+
+const robot = {
+  maxEnergy: 100,
+  currentEnergy: 100,
+  actions: [
+    {action: 'уборка', cost: 20}, 
+    {action: 'завтрак', cost: 5}, 
+    {action: 'обед', cost: 15}, 
+    {action: 'ужин', cost: 10}, 
+    {action: 'покупки', cost: 60}
+  ],
+  makeAction: function(action) {
+    let value = this.actions.find(el => el.action === action)
+    if (!value){
+      console.log(`Не могу выполнить ${action}. Такой задчи нет в моем списке.`)
+    } else if(value.cost <= this.currentEnergy) {
+      this.currentEnergy -=  value.cost
+      console.log(`Выполняю действие ${action}. Осталось ${this.currentEnergy}% заряда`)
+    } else if(value.cost > this.currentEnergy) {
+      console.log(`Недостаточно энергии для выполнения действия ${action}. Требуется ${value.cost}% энергии, осталось ${this.currentEnergy}%.`)
+    }
+  },
+  charge: function(){
+    this.currentEnergy = this.maxEnergy
+    console.log(`Заряд завершен. Уровень энергии ${this.currentEnergy}%.`)
+}
+}
+
+robot.makeAction('ужин')
+robot.makeAction('ужин')
+robot.makeAction('ужин')
+robot.makeAction('покупки')
+robot.makeAction('покупки')
+robot.charge()
+robot.makeAction('покупки')
+robot.makeAction('ужин')
+robot.charge()
+
+///////////////
 const user = {
   name: 'John',
   surname: 'Smith'
