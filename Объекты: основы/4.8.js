5c6aee1a30cbaa04aca7272939364285b4947bc6
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-04-29 20:54:40 +0700

    Update 4.8.js

--- Объекты: основы/4.8.js
+++ Объекты: основы/4.8.js
@@ -1,1 +1,33 @@
+// Как сделать, чтобы объект:
+// был равен определенному числу?
+// был равен определенной строке?
+
+const obj = {
+    [Symbol.toPrimitive](hint){
+        return hint == 'string' ?  'str' : 1;
+    }
+};
+
+console.log(+obj === 1) // true
+
+
+const obj = {
+    [Symbol.toPrimitive](hint){
+        return hint == 'string' ?  'str' : 1;
+    }
+};
+
+console.log(String(obj) === 'str') // true
+
+
+obj > 0 // true. Как это сделать?
+
+
+const obj = {
+    [Symbol.toPrimitive](hint){
+        return hint == 'string' ?  'str' : 1;
+    }
+};
+
+console.log(obj > 0) // true
 
