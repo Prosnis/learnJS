36a3bbf48af528fb34b7727ac345b512d3809c0e
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-05-01 17:15:50 +0700

    Update 5.3.js

--- Типы данных/5.3.js
+++ Типы данных/5.3.js
@@ -1,3 +1,53 @@
+// Проверка палиндрома: Напишите функцию, которая принимает строку и возвращает true, 
+// если строка является палиндромом (читается одинаково с начала и с конца), 
+// и false в противном случае. Используйте методы .toLowerCase() или .toUpperCase() для игнорирования регистра, 
+// а также методы .split(), .reverse() и .join() 
+// для разбиения строки на массив символов, переворота массива и объединения его обратно в строку.
+
+function palindrom(string){
+    let value = string.toLowerCase().split('').reverse().join('')
+    return value === string ? true : false 
+}
+// Поиск наибольшего повторяющегося подстроки:
+// Напишите функцию, которая принимает строку и возвращает наибольшую повторяющуюся подстроку в этой строке.
+// Например, для строки "abcabcd" результатом должна быть строка "abc", так как это наибольшая подстрока, повторяющаяся два раза.
+// Используйте метод .substring() для извлечения подстроки и метод .indexOf() для поиска повторений.
+
+let string = "ыврапдофывротапжщрофвыаэрфвдофалр";
+
+function subStringRepeat(string) {
+  let result = "";
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+        let subString = string.substring(i, j)
+        let currentEntries = string.indexOf(subString, j)
+
+        if (currentEntries !== -1 && result.length < subString.length) {
+            result = subString
+        }
+    }
+  }
+
+  return result
+}
+
+console.log(subStringRepeat(string))
+
+// Поиск самого длинного слова: 
+// Напишите функцию, которая принимает строку и возвращает длину самого длинного слова в этой строке. 
+// Используйте метод .split() для разбиения строки на массив слов и метод .reduce() для поиска самого длинного слова в массиве.
+
+
+function getMaxLength(string){ 
+    let arr = string.split(' ')
+    let result = arr.reduce((acc, el) => acc.length > el.length ? acc: el, 0)
+
+    return result
+}
+
+
+
 // Сделать первый символ заглавным
 // Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. 
 function ucFirst(str) {
