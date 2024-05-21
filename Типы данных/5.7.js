3ade0968c71131641b056a7ef3d2c932c6fd9f7a
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-05-21 21:22:25 +0700

    Update 5.7.js

--- Типы данных/5.7.js
+++ Типы данных/5.7.js
@@ -1,1 +1,96 @@
+map.keys() – возвращает итерируемый объект по ключам,
+map.values() – возвращает итерируемый объект по значениям,
+map.entries() – возвращает итерируемый объект по парам вида [ключ, значение], этот вариант используется по умолчанию в for..of.
+
+let recipeMap = new Map([
+  ["огурец", 500],
+  ["помидор", 350],
+  ["лук",    50]
+]);
+
+// перебор по ключам (овощи)
+for (let vegetable of recipeMap.keys()) {
+  alert(vegetable); // огурец, помидор, лук
+}
+
+// перебор по значениям (числа)
+for (let amount of recipeMap.values()) {
+  alert(amount); // 500, 350, 50
+}
+
+// перебор по элементам в формате [ключ, значение]
+for (let entry of recipeMap) { // то же самое, что и recipeMap.entries()
+  alert(entry); // огурец,500 (и так далее)
+}
+
+// выполняем функцию для каждой пары (ключ, значение)
+recipeMap.forEach((value, key, map) => {
+  alert(`${key}: ${value}`); // огурец: 500 и так далее
+});
+
+Object.entries: Map из Object
+
+let obj = {
+  name: "John",
+  age: 30
+};
+
+let map = new Map(Object.entries(obj));
+
+alert( map.get('name') ); // John
+
+Object.fromEntries: Object из Map
+
+let map = new Map();
+map.set('banana', 1);
+map.set('orange', 2);
+map.set('meat', 4);
+
+let obj = Object.fromEntries(map);; // создаём обычный объект (*)
+
+// готово!
+// obj = { banana: 1, orange: 2, meat: 4 }
+
+alert(obj.orange); // 2
+
+
+Set
+Объект Set – это особый вид коллекции: «множество» значений (без ключей), где каждое значение может появляться только один раз.
+
+Его основные методы это:
+
+new Set(iterable) – создаёт Set, и если в качестве аргумента был предоставлен итерируемый объект (обычно это массив), то копирует его значения в новый Set.
+set.add(value) – добавляет значение (если оно уже есть, то ничего не делает), возвращает тот же объект set.
+set.delete(value) – удаляет значение, возвращает true, если value было в множестве на момент вызова, иначе false.
+set.has(value) – возвращает true, если значение присутствует в множестве, иначе false.
+set.clear() – удаляет все имеющиеся значения.
+set.size – возвращает количество элементов в множестве.
+
+// Фильтрация уникальных элементов массива
+function unique(arr) {
+    return Array.from(new Set(arr)).join(',')
+  }
+  
+  let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  console.log( unique(values) ); // Hare,Krishna,:-O
+
+// Отфильтруйте анаграммы
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+  let result = [];
+  for (let el of arr) {
+    result.push(el.toLowerCase()
+          .split("")
+          .sort()
+          .join(""));
+  }
+  return Array.from(new Set(result));
+}
+
+console.log(aclean(arr));
+
 
