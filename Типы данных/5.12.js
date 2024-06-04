8413d9aca3e33cba02e8237363a30ca37e98d283
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-06-04 18:23:50 +0700

    Update 5.12.js

--- Типы данных/5.12.js
+++ Типы данных/5.12.js
@@ -1,1 +1,48 @@
+// При кодировании пропускает:
+undefined 
+symbol
+method()
 
+
+// Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+let user2 = JSON.parse(JSON.stringify(user));
+
+
+let room = {
+    number: 23
+  };
+  
+  let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+  };
+  
+  // цикличные ссылки
+  room.occupiedBy = meetup;
+  meetup.self = meetup;
+  
+  console.log( JSON.stringify(meetup, function replacer(key, value) {
+    return key != "" && value == meetup ? undefined : value
+  }));
+
+
+  
+  /* в результате должно быть:
+  {
+    "title":"Совещание",
+    "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+    "place":{"number":23}
+  }
+  */
