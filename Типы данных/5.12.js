e9d30d4e8bf5877f3a975d1baa2e68dceae72c36
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-06-04 18:28:52 +0700

    Update 5.12.js

--- Типы данных/5.12.js
+++ Типы данных/5.12.js
@@ -1,3 +1,57 @@
+// JSON.stringify(value[, replacer, space])
+// value -  Значение для кодирования.
+// replacer - Массив свойств для кодирования или функция соответствия function(key, value).
+// space - Дополнительное пространство (отступы), используемое для форматирования.
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: [{name: "John"}, {name: "Alice"}],
+  place: room // meetup ссылается на room
+};
+
+room.occupiedBy = meetup; // room ссылается на meetup
+
+alert( JSON.stringify(meetup, ['title', 'participants']) );
+// {"title":"Conference","participants":[{},{}]}
+
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: [{name: "John"}, {name: "Alice"}],
+  place: room // meetup ссылается на room
+};
+
+room.occupiedBy = meetup; // room ссылается на meetup
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  alert(`${key}: ${value}`);
+  return (key == 'occupiedBy') ? undefined : value;
+}));
+
+
+// JSON.parse(str[, reviver]);
+// str - JSON для преобразования в объект.
+// reviver - Необязательная функция, которая будет вызываться для каждой пары (ключ, значение) и может преобразовывать значение.
+
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+let meetup = JSON.parse(str, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+alert( meetup.date.getDate() ); 
+
+
+
 // При кодировании пропускает:
 undefined 
 symbol
