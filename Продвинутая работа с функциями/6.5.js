90c42fe477449031de4ba643bf5ae491885306d7
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-06-13 14:59:36 +0700

    Update 6.5.js

--- Продвинутая работа с функциями/6.5.js
+++ Продвинутая работа с функциями/6.5.js
@@ -1,1 +1,22 @@
+var one = 'one' // window.one => 'one'
+let two = 'two' //window.two => undefined
+var three = 'three' //window.three => undefined
 
+
+function declaration(a = 10, b =10){
+  return a+b
+} // window.declaration() => 20
+
+const expression = function (a =10, b =10){
+  return a+b
+} // window.expression() => undefined
+
+const arrow = () => {
+  return 10+10
+} // window.arrow() => undefined
+
+
+window.one = 'one'
+
+console.log(window.one) // 'one' - тоже самое что и 'var one = 'one''
+console.log(one) // one
