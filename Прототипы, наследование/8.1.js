ac2cd8d5373c7f98e49e9fe54838bcd97390ae03
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-07-20 15:22:11 +0700

    Update 8.1.js

--- Прототипы, наследование/8.1.js
+++ Прототипы, наследование/8.1.js
@@ -1,1 +1,66 @@
+let animal = {
+  jumps: null
+};
 
+let rabbit = {
+  __proto__: animal,
+  jumps: true
+};
+
+console.log( rabbit.jumps ); // ? (1) true
+
+delete rabbit.jumps;
+
+console.log( rabbit.jumps ); // ? (2) null
+
+delete animal.jumps;
+
+console.log( rabbit.jumps ); // ? (3) undefined
+
+
+// С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся 
+// по следующему пути: pockets → bed → table → head. 
+// Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
+
+// Ответьте на вопрос: как быстрее получить значение glasses – через pockets.glasses или через head.glasses? При необходимости составьте цепочки поиска и сравните их.
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3,
+  __proto__: head
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed
+};
+
+console.log(pockets.pen) // 3
+console.log(bed.glasses) // 1
+
+
+// Объект rabbit наследует от объекта animal.
+// Какой объект получит свойство full при вызове rabbit.eat(): animal или rabbit?
+
+let animal = {
+  eat() {
+    this.full = true;
+  }
+};
+
+let rabbit = {
+  __proto__: animal
+};
+
+
+rabbit.eat()
+console.log(rabbit.full) // true
