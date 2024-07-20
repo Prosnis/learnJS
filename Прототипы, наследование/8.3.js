20090d3bd7c76817b3c1e9b20e4bb54272048cd5
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-07-20 15:57:43 +0700

    Update 8.3.js

--- Прототипы, наследование/8.3.js
+++ Прототипы, наследование/8.3.js
@@ -1,1 +1,37 @@
+const obj = {}
+const number = 5
+const string = 'string'
+const array = []
+function test(){}
 
+console.log(obj.__proto__ == Object.prototype) // true
+console.log(number.__proto__ == Number.prototype) // true
+console.log(string.__proto__ == String.prototype) // true
+console.log(array.__proto__ == Array.prototype) // true
+console.log(test.__proto__ == Function.prototype) // true
+
+
+
+function f() {
+  console.log("Hello!");
+}
+
+Function.prototype.defer = function(ms){
+  return setTimeout(this, ms)
+}
+
+f.defer(1000); // выведет "Hello!" через 1 секунду
+
+
+
+Function.prototype.defer = function(ms){
+  return function(...args){
+    setTimeout(()=> f.apply(this, args), ms)
+  }
+}
+
+function f(a, b) {
+  console.log( a + b );
+}
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
