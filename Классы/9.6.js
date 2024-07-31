16389f66c39176d77cbc6d53f72591e6200aabf8
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-07-31 20:59:04 +0700

    Update 9.6.js

--- Классы/9.6.js
+++ Классы/9.6.js
@@ -1,1 +1,66 @@
 
+class Rabbit {}
+let rabbit = new Rabbit();
+
+// это объект класса Rabbit?
+alert( rabbit instanceof Rabbit ); // true
+
+// вместо класса
+function Rabbit() {}
+
+alert( new Rabbit() instanceof Rabbit ); // true
+
+
+let arr = [1, 2, 3];
+alert( arr instanceof Array ); // true
+alert( arr instanceof Object ); // true
+
+
+// проверка instanceof будет полагать,
+// что всё со свойством canEat - животное Animal
+class Animal {
+  static [Symbol.hasInstance](obj) {
+    if (obj.canEat) return true;
+  }
+}
+
+let obj = { canEat: true };
+alert(obj instanceof Animal); // true: вызван Animal[Symbol.hasInstance](obj)
+
+
+obj.__proto__ === Class.prototype?
+obj.__proto__.__proto__ === Class.prototype?
+obj.__proto__.__proto__.__proto__ === Class.prototype?
+...
+// если какой-то из ответов true - возвратить true
+// если дошли до конца цепочки - false
+
+function Rabbit() {}
+let rabbit = new Rabbit();
+
+// заменяем прототип
+Rabbit.prototype = {};
+
+// ...больше не rabbit!
+alert( rabbit instanceof Rabbit ); // false
+
+
+let s = Object.prototype.toString;
+
+alert( s.call(123) ); // [object Number]
+alert( s.call(null) ); // [object Null]
+alert( s.call(alert) ); // [object Function]
+
+
+let user = {
+  [Symbol.toStringTag]: "User"
+};
+
+alert( {}.toString.call(user) ); // [object User]
+
+// toStringTag для браузерного объекта и класса
+alert( window[Symbol.toStringTag]); // window
+alert( XMLHttpRequest.prototype[Symbol.toStringTag] ); // XMLHttpRequest
+
+alert( {}.toString.call(window) ); // [object Window]
+alert( {}.toString.call(new XMLHttpRequest()) ); // [object XMLHttpRequest]
