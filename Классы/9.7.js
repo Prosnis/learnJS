0ec0c900fc64e4a3afc6fc2ab64163d05198494d
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-07-31 21:03:09 +0700

    Update and rename 9.7,js to 9.7.js

---
+++ Классы/9.7.js
@@ -0,0 +1,29 @@
+let sayMixin = {
+  say(phrase) {
+    alert(phrase);
+  }
+};
+
+let sayHiMixin = {
+  __proto__: sayMixin, // (или мы можем использовать Object.setPrototypeOf для задания прототипа)
+
+  sayHi() {
+    // вызываем метод родителя
+    super.say(`Привет, ${this.name}`); // (*)
+  },
+  sayBye() {
+    super.say(`Пока, ${this.name}`); // (*)
+  }
+};
+
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// копируем методы
+Object.assign(User.prototype, sayHiMixin);
+
+// теперь User может сказать Привет
+new User("Вася").sayHi(); // Привет, Вася!
