1e9958eedebaa0bbb8bb90870d5346ceb5caca25
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-08-27 16:43:48 +0700

    Update 11.2.js

--- Промисы/11.2.js
+++ Промисы/11.2.js
@@ -1,1 +1,31 @@
 
+
+function test(id) {
+    fetch(`https://jsonplaceholder.typicode.com/comments/${id}`) // возращает промис
+      .then(response => { // получаем ответ 
+        if (!response.ok) { // статус в диапозоне 200-299 - ок, если нет, то выбразываем ошибку 
+          throw new Error(`HTTP error! status: ${response.status}`); 
+        }
+        return response.json(); // метод .json() декодирует полученный ответ из формата json 
+      })
+      .then(res => console.log(res)) // при response.ok обрабатываем результат
+      .catch(error => { // обрабатываем ошибку 
+        console.error('Fetch error:', error); 
+      });
+  }
+  
+  test(5000);
+
+
+// начальное состояние промиса
+state: "pending"  
+result: undefined
+
+// состояние промиса в случае ошибки 
+state: "rejected" 
+result: error
+
+
+// состояние промиса в случае успешного выполнения 
+state: "fulfilled"
+result: value
