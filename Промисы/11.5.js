50e14bf57b4ce6eb20d6f157ffdf09c8058ec3a8
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-08-29 12:33:14 +0700

    Update 11.5.js

--- Промисы/11.5.js
+++ Промисы/11.5.js
@@ -35,14 +35,6 @@ Promise.all([
 
 //////////////////
 
-Promise.all([
-  new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
-  new Promise((resolve, reject) => setTimeout(() => reject(new Error("Ошибка!")), 2000)), // 
-  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
-]).catch(alert); // Error: Ошибка!
-
-/////////////////
-
 const urls = [
   "https://jsonplaceholder.typicode.com/users/1",
   "https://jsonplaceholder.typicode.com/users/5235235",
