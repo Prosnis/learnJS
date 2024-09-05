fdbbc337435c2e3379d4bfeb408c15dd4f1d5594
Prosnis <144787794+Prosnis@users.noreply.github.com>
2024-09-05 15:26:49 +0700

    Update 12.2.js

--- Генераторы, продвинутая итерация/12.2.js
+++ Генераторы, продвинутая итерация/12.2.js
@@ -1,1 +1,30 @@
 
+getSwapiPagerator = (endpoint) => 
+  async function* () {
+    let nextUrl = `http://swapi.dev/api/${endpoint}`;
+    while (nextUrl) {
+      const response = await fetch(nextUrl);
+      const data = await response.json();
+      nextUrl = data.next; 
+      yield* data.results; 
+    }
+  };
+
+
+starWars = {
+  planets: {
+    [Symbol.asyncIterator]: getSwapiPagerator('planets')
+  }
+};
+
+const results = [];
+
+(async () => { 
+
+  for await (const planet of starWars.planets) {
+    console.log(planet.name);
+    results.push(planet.name);
+  }
+  console.log('All planets:', results); 
+})();
+
